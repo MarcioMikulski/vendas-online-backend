7ca75a2b75024ef3acea3a3fd2afe23c48c5fad6
Marcio Mikulski <mugemikulski@gmail.com>
2023-04-15 14:28:00 -0300

    feat: create token JWT

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,16 +5,16 @@ import {
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
-import { ReturnUserDto } from 'src/user/dtos/returnUser.dto';
 import { LoginDto } from './dtos/login.dto';
 import { AuthService } from './auth.service';
+import { ReturnLogin } from './dtos/returnLogin.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
   @UsePipes(ValidationPipe)
   @Post()
-  async login(@Body() loginDto: LoginDto): Promise<ReturnUserDto> {
-    return new ReturnUserDto(await this.authService.login(loginDto));
+  async login(@Body() loginDto: LoginDto): Promise<ReturnLogin> {
+    return this.authService.login(loginDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,12 +3,19 @@ import { UserEntity } from 'src/user/entities/user.entity';
 import { UserService } from 'src/user/user.service';
 import { LoginDto } from './dtos/login.dto';
 import { compare } from 'bcrypt';
+import { ReturnLogin } from './dtos/returnLogin.dto';
+import { ReturnUserDto } from 'src/user/dtos/returnUser.dto';
+import { LoginPayload } from './dtos/loginPayload.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
-  async login(loginDto: LoginDto): Promise<UserEntity> {
+  async login(loginDto: LoginDto): Promise<ReturnLogin> {
     const user: UserEntity | undefined = await this.userService
       .findUserByEmail(loginDto.email)
       .catch(() => undefined);
@@ -18,6 +25,9 @@ export class AuthService {
     if (!user || !isMatch) {
       throw new NotFoundException('Email ou Password invalid');
     }
-    return user;
+    return {
+      accessToken: this.jwtService.sign({ ...new LoginPayload(user) }),
+      user: new ReturnUserDto(user),
+    };
   }
 }
