af874144a2de009f17adb40542dc4b56dcc6af1d
Marcio Mikulski <mugemikulski@gmail.com>
2023-04-14 21:02:24 -0300

    feat: creat module auth

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { UserEntity } from 'src/user/entities/user.entity';
+import { UserService } from 'src/user/user.service';
+import { LoginDto } from './dtos/login.dto';
+import { compare } from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly userService: UserService) {}
+
+  async login(loginDto: LoginDto): Promise<UserEntity> {
+    const user: UserEntity | undefined = await this.userService
+      .findUserByEmail(loginDto.email)
+      .catch(() => undefined);
+
+    const isMatch = await compare(loginDto.password, user?.password || '');
+
+    if (!user || !isMatch) {
+      throw new NotFoundException('Email ou Password invalid');
+    }
+    return user;
+  }
+}
